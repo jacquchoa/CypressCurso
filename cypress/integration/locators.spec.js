66d4c2928a37eb908847de2d495e64a5101e7501
jacquchoa <fjacqfu@gmail.com>
2023-06-02 16:59:27 -0300

    alteração no documento locators

--- cypress/integration/locators.spec.js
+++ cypress/integration/locators.spec.js
@@ -1,7 +1,7 @@
 ///<reference types="cypress"/>
 
 //Seção 7 aula 41 e 42
-
+describe('Desafio',() => {
 before(() => {
     cy.visit('https://wcaquino.me/cypress/componentes.html')
 })
@@ -10,7 +10,6 @@ beforeEach(() => {
     cy.reload() 
 })
 
-describe('Desafio',() => {
     it('Using JQuery Selector', () => {
         cy.get(':nth-child(1) > :nth-child(3) > [type="button"]')
         cy.get('table#tabelaUsuarios tbody > tr:eq(0) td:nth-child(3) > input')
@@ -21,8 +20,9 @@ describe('Desafio',() => {
 
     it('Using xpath', () => {
 // apresentando TypeError: cy.xpath is not a function 
-    //    cy.xpath('//input[contains(@onclick,\'Francisco\')]')
+        cy.xpath('//input[contains(@onclick,\'Francisco\')]')
     //    cy.xpath("//table[@id='tabelaUsuarios']//td[contains(.,'Francisco)]/.,//input[@type='text']")
     //    cy.xpath("//to[contains(.,'Usuario A')]/following-sibling;;td[constains(.,'Mestrado')]/..//input(@type='text').type('Funciona')")
     })
 })
+
