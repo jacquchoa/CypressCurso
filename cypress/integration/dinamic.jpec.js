bae2f8b49e5487b2f2a26e7da69606353fc85f9b
jacquchoa <fjacqfu@gmail.com>
2023-09-05 12:19:21 -0300

    adicionando a pasta dinamic

--- cypress/integration/dinamic.jpec.js
+++ cypress/integration/dinamic.jpec.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 //seção 8 aula 46 - Teste dinamico
+//seção 8 aula 47 - Teste dinamico
 
 describe('Dinamic test', () => {
     beforeEach(() => {
@@ -21,5 +22,28 @@ foods.forEach(food => {
         cy.get('#formCadastrar').click()
         cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
     })
+})
+
+it.only('Deve selecionar todos usando o each', () => {
+    cy.get('#formNome').type('Usuario')
+    cy.get('#formSobrenome').type('Qualquer')
+    cy.get(`[name=formSexo][value=F]`).click()
+    // cy.get('[name="formComidaFavorita"]').click({ multiple: true })
+    
+    //Usando a funcionalidade '$el'
+    //Para selecionar as opções não celecionando a opção vegetariano
+    cy.get('[name="formComidaFavorita"]').each($el => {
+        //$el.click()
+        if($el.val() != 'vegetariano')
+        cy.wrap($el).click()
+    })
+
+    cy.get('#formEscolaridade').select('Doutorado')
+    cy.get('#formEsportes').select('Corrida')
+    cy.get('#formCadastrar').click()
+    cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
+    
+    ///CLICKALERT esta apresentando erro, não encontra 'clickAlert' apos limpeza no notebook
+    //cy.clickAlert('#formCadastrar', 'Tem certeza que voce eh vegetariano?')
 })
 })
